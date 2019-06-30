35917c3573ee991d7c3c8d93e919cc255a775a80
孙旭 <34908553+sunxu654@users.noreply.github.com>
2019-06-30 21:31:13 +0800

    增加Redis业务类,包装redisTemplate.opsFunction

---
+++ src/main/java/com/sxfdu/redis/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.sxfdu.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author sunxu93@163.com
+ * @date 19/6/30/030 17:27
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        return  redisTemplate;
+    }
+
+}
