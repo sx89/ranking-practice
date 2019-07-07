9489e72fe6b475dc05f4199a7cba518272483b03
孙旭 <34908553+sunxu654@users.noreply.github.com>
2019-07-07 17:27:26 +0800

    增加Controller层

--- src/main/java/com/sxfdu/redis/controller/RankingController.java
+++ src/main/java/com/sxfdu/redis/controller/RankingController.java
@@ -1,6 +1,15 @@
 package com.sxfdu.redis.controller;
 
+import com.sxfdu.redis.service.RankingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author sunxu93@163.com
@@ -8,5 +17,45 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class RankingController {
+
+
+    @Autowired
+    private RankingService rankingService;
+
+    @ResponseBody
+    @RequestMapping("/addScore")
+    public String addRank(String uid, Integer score) {
+        rankingService.rankAdd(uid, score);
+        return "success";
+    }
+
+
+
+    @ResponseBody
+    @RequestMapping("/increScore")
+    public String increScore(String uid, Integer score) {
+        rankingService.increSocre(uid, score);
+        return "success";
+    }
+
+    @ResponseBody
+    @RequestMapping("/rank")
+    public Map<String, Long> rank(String uid) {
+        Map<String, Long> map = new HashMap<>();
+        map.put(uid, rankingService.rankNum(uid));
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("/score")
+    public Long rankNum(String uid) {
+        return rankingService.score(uid);
+    }
+
+    @ResponseBody
+    @RequestMapping("/scoreByRange")
+    public Set<ZSetOperations.TypedTuple<Object>> scoreByRange(Integer start, Integer end) {
+        return rankingService.rankWithScore(start,end);
+    }
 
 }
