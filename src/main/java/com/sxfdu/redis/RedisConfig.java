d8a6e6823472be1eb1ef52a710b824d8a69ea48f
孙旭 <sunxufdu@163.com>
2019-07-13 12:34:48 +0800

    序列化方式改进

--- src/main/java/com/sxfdu/redis/RedisConfig.java
+++ src/main/java/com/sxfdu/redis/RedisConfig.java
@@ -31,24 +31,28 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(factory);
+            RedisTemplate<String, String> redisTemplate = new RedisTemplate<String,String>();
+            redisTemplate.setConnectionFactory(factory);
+            // 使用Jackson2JsonRedisSerialize 替换默认序列化
+            /**Jackson序列化  json占用的内存最小 */
+            Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+            ObjectMapper om = new ObjectMapper();
+            om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+            om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+            jackson2JsonRedisSerializer.setObjectMapper(om);
+            /**Jdk序列化   JdkSerializationRedisSerializer是最高效的*/
+//      JdkSerializationRedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
+            /**String序列化*/
+            StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+            /**将key value 进行stringRedisSerializer序列化*/
+            redisTemplate.setKeySerializer(stringRedisSerializer);
+            redisTemplate.setValueSerializer(stringRedisSerializer);
+            /**将HashKey HashValue 进行序列化*/
+            redisTemplate.setHashKeySerializer(stringRedisSerializer);
+            redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+            redisTemplate.afterPropertiesSet();
 
-        // 使用Jackson2JsonRedisSerialize 替换默认序列化
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-
-        // 设置value的序列化规则和 key的序列化规则
-        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.afterPropertiesSet();
-
-        return redisTemplate;
+            return redisTemplate;
     }
 
     @Bean
